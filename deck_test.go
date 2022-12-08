fef0b22f376faa5924caa84ba9f412a6e77a0f6d
Maanas M Singh <maanasmsingh@gmail.com>
2022-12-08 05:30:46 +0530

    initial commit

---
+++ deck_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Hearts" {
+		t.Errorf("Expected last card of Four of Hearts, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	err := os.Remove("_decktesting")
+	if err != nil {
+		return
+	}
+
+	deck := newDeck()
+	err = deck.saveToFile("_decktesting")
+	if err != nil {
+		return
+	}
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	err = os.Remove("_decktesting")
+	if err != nil {
+		return
+	}
+}
